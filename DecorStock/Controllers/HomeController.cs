c9ca057370fc6c7d758d6d1be560e5cd172917e6
Katlego <kdawgg28@gmail.com>
2024-10-14 18:50:08 +0200

    Add project files.

---
+++ DecorStock/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using DecorStock.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DecorStock.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Items()
+        {
+            return View();
+        }
+
+        public IActionResult CreateEditItems()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DecorStock/Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DecorStock.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        public string ItemName { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+
+
+    }
+}
